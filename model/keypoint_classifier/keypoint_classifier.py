037936804d23350f92d038d122e810c498f116e2
bimal-tech <074bct107@nce.edu.np>
2022-02-27 16:22:17 +0545

    formated code by bimal

--- model/keypoint_classifier/keypoint_classifier.py
+++ model/keypoint_classifier/keypoint_classifier.py
@@ -4,9 +4,9 @@ import tensorflow as tf
 
 class KeyPointClassifier(object):
     def __init__(
-        self,
-        model_path='model/keypoint_classifier/keypoint_classifier.tflite',
-        num_threads=1,
+            self,
+            model_path='model/keypoint_classifier/keypoint_classifier.tflite',
+            num_threads=1,
     ):
         self.interpreter = tf.lite.Interpreter(model_path=model_path,
                                                num_threads=num_threads)
@@ -16,8 +16,8 @@ class KeyPointClassifier(object):
         self.output_details = self.interpreter.get_output_details()
 
     def __call__(
-        self,
-        landmark_list,
+            self,
+            landmark_list,
     ):
         input_details_tensor_index = self.input_details[0]['index']
         self.interpreter.set_tensor(
@@ -28,7 +28,11 @@ class KeyPointClassifier(object):
         output_details_tensor_index = self.output_details[0]['index']
 
         result = self.interpreter.get_tensor(output_details_tensor_index)
-
+        # for res in result:
+        #     for r in res:
+        #         print(float(r)*100.0000)
+        #
         result_index = np.argmax(np.squeeze(result))
-
-        return result_index
+        probab = np.squeeze(result)[result_index]
+        # print("---------------------------------------------------------------")
+        return result_index, probab
